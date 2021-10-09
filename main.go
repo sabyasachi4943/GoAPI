45c47e9f68d0dc8f0baf7c3e798191eb92e9a081
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2021-10-09 21:12:31 +0600

    custom error message

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Asset not found\n"))
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Running API v1\n"))
 }
